da61b2726ea7d43c199233539d13066dcdc1d009
joshs <samsonknight909@gmail.com>
2025-09-15 12:47:45 -0400

    Cleaned up code added bonus test

--- src/main/java/org/example/Scanner.java
+++ src/main/java/org/example/Scanner.java
@@ -1,2 +1,116 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
+
+    enum TOKEN {
+        SCANEOF, ID, INTLITERAL, VAR, WRITE, INIT, EQUALS,
+        NOTEQUALS, IF, THEN, ENDIF, WHILE, DO,
+        ENDWHILE, CALCULATE, PLUS;
+    }
+
+    PushbackReader pbr;
+
+    List<String> reservedWords;
+
+
+
+    public Scanner(PushbackReader pbr) {
+        this.pbr = pbr;
+        reservedWords = new ArrayList<String>();
+        reservedWords.add("var");
+        reservedWords.add("write");
+        reservedWords.add("init");
+        reservedWords.add("if");
+        reservedWords.add("then");
+        reservedWords.add("endif");
+        reservedWords.add("calculate");
+        reservedWords.add("do");
+        reservedWords.add("while");
+        reservedWords.add("endwhile");
+
+    }
+
+    StringBuilder tokenBuffer = new StringBuilder();
+
+    TOKEN scan() throws IOException {
+        tokenBuffer.setLength(0);
+        int c;
+        c = pbr.read();
+
+        while (c != -1 && c!=65535) {
+
+            if (c == 32 || c == 9 || c == 10 || c == 13) {
+                tokenBuffer.setLength(0);
+                c = pbr.read();
+                continue;
+            }
+            tokenBuffer.append((char) c);
+
+
+            if (Character.isLetter((char) c)) {
+                c = pbr.read();
+                while (Character.isLetterOrDigit((char) c)) {
+                    tokenBuffer.append((char) c);
+                    c = pbr.read();
+                }
+
+                pbr.unread(c);
+                if (reservedWords.contains(tokenBuffer.toString())) {
+
+                    return TOKEN.valueOf(tokenBuffer.toString().toUpperCase());
+
+
+
+                }
+                else {
+
+                    return TOKEN.ID;
+                }
+
+            } else if (c == '!') {
+
+                c = pbr.read();
+                if (c == '=') {
+
+                    return TOKEN.NOTEQUALS;
+                }
+            } else if (c == '+') {
+
+                return TOKEN.PLUS;
+            } else if (c == '=') {
+
+                return TOKEN.EQUALS;
+            } else if (Character.isDigit((char) c)) {
+                c = pbr.read();
+
+                while (Character.isDigit((char) c) && c!=-1) {
+                    tokenBuffer.append((char) c);
+                    c = pbr.read();
+
+                }
+                pbr.unread(c);
+                return TOKEN.INTLITERAL;
+            }
+            else{
+                c = pbr.read();
+
+            }
+
+        }
+        return TOKEN.SCANEOF;
+    }
+
+    String getTokenBufferString() {
+        return tokenBuffer.toString();
+
+    }
+
 }
+
+
+
--- src/test/java/org/example/ScannerTest.java
+++ src/test/java/org/example/ScannerTest.java
@@ -1,4 +1,253 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ScannerTest {
-  
+    @Test
+    public void test1() throws IOException {
+
+        String s = "var score123";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.VAR, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("score123", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testC);
+
+    }
+    @Test
+    public void test2() throws IOException {
+        String s = "init score = 600";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.INIT, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("score", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.EQUALS, testC);
+
+        Scanner.TOKEN testD = scanObj.scan();
+        assertEquals(Scanner.TOKEN.INTLITERAL, testD);
+        assertEquals("600", scanObj.getTokenBufferString());
+
+
+        Scanner.TOKEN testE = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testE);
+    }
+
+    @Test
+    public void test3() throws IOException {
+        String s = "calculate newsalary = originalsalary + raise";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.CALCULATE, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("newsalary", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.EQUALS, testC);
+
+        Scanner.TOKEN testD = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testD);
+        assertEquals("originalsalary", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testE = scanObj.scan();
+        assertEquals(Scanner.TOKEN.PLUS, testE);
+
+
+        Scanner.TOKEN testF = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testF);
+        assertEquals("raise", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testG = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testG);
+    }
+    @Test
+    public void test4() throws IOException {
+        String s = "write salary";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.WRITE, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("salary", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testC);
+
+    }
+    @Test
+    public void test5() throws IOException {
+        String s = "if x = y then endif";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.IF, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("x", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.EQUALS, testC);
+
+        Scanner.TOKEN testD = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testD);
+        assertEquals("y", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testE = scanObj.scan();
+        assertEquals(Scanner.TOKEN.THEN, testE);
+
+        Scanner.TOKEN testF = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ENDIF, testF);
+
+        Scanner.TOKEN testG = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testG);
+    }
+    @Test
+    public void test6() throws IOException {
+        String s = "if x = y then write x endif";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.IF, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("x", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.EQUALS, testC);
+
+        Scanner.TOKEN testD = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testD);
+        assertEquals("y", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testE = scanObj.scan();
+        assertEquals(Scanner.TOKEN.THEN, testE);
+
+        Scanner.TOKEN testF = scanObj.scan();
+        assertEquals(Scanner.TOKEN.WRITE, testF);
+
+        Scanner.TOKEN testG = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testG);
+        assertEquals("x", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testH = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ENDIF, testH);
+
+        Scanner.TOKEN testJ = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testJ);
+    }
+    @Test
+    public void test7() throws IOException {
+        String s = "while x != y do calculate x = x + 1 endwhile";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.WHILE, testA);
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testB);
+        assertEquals("x", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.NOTEQUALS, testC);
+
+        Scanner.TOKEN testD = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testD);
+        assertEquals("y", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testE = scanObj.scan();
+        assertEquals(Scanner.TOKEN.DO, testE);
+
+        Scanner.TOKEN testF = scanObj.scan();
+        assertEquals(Scanner.TOKEN.CALCULATE, testF);
+
+        Scanner.TOKEN testG = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testG);
+        assertEquals("x", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testH = scanObj.scan();
+        assertEquals(Scanner.TOKEN.EQUALS, testH);
+
+        Scanner.TOKEN testI = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testI);
+        assertEquals("x", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testJ = scanObj.scan();
+        assertEquals(Scanner.TOKEN.PLUS, testJ);
+
+        Scanner.TOKEN testK = scanObj.scan();
+        assertEquals(Scanner.TOKEN.INTLITERAL, testK);
+        assertEquals("1", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testL = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ENDWHILE, testL);
+
+        Scanner.TOKEN testM = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testM);
+
+    }
+    @Test
+    public void test8() throws IOException {
+        String s = "";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.SCANEOF, testA);
+    }
+    //Made a test to check input with no spaces
+    @Test
+    public void test9() throws IOException {
+        String s = "Num100+10!=110";
+        StringReader sr = new StringReader(s);
+        Scanner scanObj = new Scanner(new PushbackReader(sr));
+        Scanner.TOKEN testA = scanObj.scan();
+        assertEquals(Scanner.TOKEN.ID, testA);
+        assertEquals("Num100", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testB = scanObj.scan();
+        assertEquals(Scanner.TOKEN.PLUS, testB);
+
+        Scanner.TOKEN testC = scanObj.scan();
+        assertEquals(Scanner.TOKEN.INTLITERAL, testC);
+        assertEquals("10", scanObj.getTokenBufferString());
+
+        Scanner.TOKEN testD = scanObj.scan();
+        assertEquals(Scanner.TOKEN.NOTEQUALS, testD);
+
+        Scanner.TOKEN testE = scanObj.scan();
+        assertEquals(Scanner.TOKEN.INTLITERAL, testE);
+        assertEquals("110", scanObj.getTokenBufferString());
+
+    }
+
 }
